a13b8708ae435bb5353ced7f44a7161a247322d7
Tom Bamford <tom@bamford.io>
2025-04-16 00:15:41 +0100

    Prettify scripts output, add warning for proxy settings, perform actual connection test isntead of waiting 120 seconds for new database

--- scripts/lima-k3s.sh
+++ scripts/lima-k3s.sh
@@ -3,53 +3,79 @@
 REPO_DIR="$(cd "$(dirname $0)"/.. && pwd)"
 IMPORTS_DIR="${DEFRA_WORKSPACE}"
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 if ! [[ -d "${DEFRA_WORKSPACE}" ]]; then
-  echo "DEFRA_WORKSPACE environment variable not set." >&2
-  echo >&2
-  echo "Please set this to the directory where the \`imports\` repositories are checked out." >&2
+  echo -e "${RED}Error: DEFRA_WORKSPACE environment variable not set.${NC}" >&2
+  echo "Please specify the directory where the \`imports\` repositories are checked out, using the \`DEFRA_WORKSPACE\` environment variable." >&2
   echo "e.g. \`export DEFRA_WORKSPACE=/path/to/imports\`" >&2
+  echo >&2
   exit 1
+fi
+
+if [[ "${http_proxy}${https_proxy}" != "" ]]; then
+  echo -e "${YELLOW}Warning: You have set the \`http_proxy\` or \`https_proxy\` environment variable(s).${NC}" >&2
+  echo "These will be translated and set in your IPAFFS virtual machine by Lima. This may or may not be what you want, noting that any TLS interception is likely to break provisioning of the VM. To prevent this, simply unset both of these environment variables and re-provision the VM:" >&2
+  echo "  $ unset http_proxy https_proxy" >&2
+  echo "  $ limactl stop ipaffs" >&2
+  echo "  $ limactl delete ipaffs" >&2
+  echo "  $ ${0}" >&2
+  echo >&2
 fi
 
 set -e
 
 # Check prerequisites
 if ! command -v limactl >/dev/null 2>&1; then
-  echo "\`limactl\` not available in PATH. Please install Lima" >&2
+  echo -e "${RED}Error: \`limactl\` not available in PATH. Please install Lima.${NC}" >&2
   exit 1
 fi
 if ! command -v kubectl >/dev/null 2>&1; then
-  echo "\`kubectl\` not available in PATH. Please install Kubernetes command-line interface" >&2
+  echo -e "${RED}Error: \`kubectl\` not available in PATH. Please install Kubernetes command-line interface.${NC}" >&2
   exit 1
 fi
 if ! command -v openssl >/dev/null 2>&1; then
-  echo "\`openssl\` not available in PATH. Please install OpenSSL" >&2
+  echo -e "${RED}Error: \`openssl\` not available in PATH. Please install OpenSSL.${NC}" >&2
   exit 1
 fi
 
 # Create VM using lima if not already present
 if limactl list ipaffs 2>&1 | grep -q "No instance"; then
+  echo -e "${BLUE}:: Creating virtual machine using \`limactl\`${NC}"
   [[ "$(uname)" == "Darwin" ]] && VZARGS="--vm-type=vz --rosetta"
   limactl create --name=ipaffs ${VZARGS} --tty=false "${REPO_DIR}/lima/k3s-local-dev.yaml"
 fi
 
 # Start the VM if not running
 if ! limactl list ipaffs | grep -q Running; then
+  echo -e "${BLUE}:: Starting virtual machine using \`limactl\`${NC}"
   ## allow privileged port access for none sudo user
   [[ "$(uname)" == "Linux" ]] && sudo setcap 'cap_net_bind_service=+ep' /usr/bin/ssh
 
   limactl start ipaffs
 fi
 
+# Use correct Docker context
+echo -e "${BLUE}:: Switching Docker context to \`lima-ipaffs\`${NC}"
+docker context use lima-ipaffs
+
 # Generate TLS certificate and key if missing
 if ! [[ -e "${REPO_DIR}/deploy/tls/imp.dev.azure.defra.cloud.pem" ]]; then
+  echo -e "${BLUE}:: Creating self-signed wildcard certificate${NC}"
   openssl req -x509 -nodes -newkey rsa:2048 -days 3650 \
     -keyout "${REPO_DIR}/deploy/tls/imp.dev.azure.defra.cloud-key.pem" \
     -out "${REPO_DIR}/deploy/tls/imp.dev.azure.defra.cloud.pem" \
     -subj "/C=GB/ST=England/L=Leeds/O=DEFRA/OU=IPAFFS/CN=*.imp.dev.azure.defra.cloud"
 
   # Trust the certificate on macOS
-  [[ "$(uname)" == "Darwin" ]] && security add-trusted-cert -d -r trustRoot -k "${HOME}/Library/Keychains/login.keychain-db" "${REPO_DIR}/deploy/tls/imp.dev.azure.defra.cloud.pem"
+  if [[ "$(uname)" == "Darwin" ]]; then
+    echo -e "${BLUE}:: Adding the self-signed certificate to the MacOS Login Keychain${NC}"
+    security -v add-trusted-cert -d -r trustRoot -k "${HOME}/Library/Keychains/login.keychain-db" "${REPO_DIR}/deploy/tls/imp.dev.azure.defra.cloud.pem"
+  fi
 fi
 
 # Create persistent storage directory for registry
@@ -59,6 +85,16 @@ limactl shell ipaffs sudo mkdir -p /srv/registry
 export KUBECONFIG="${HOME}/.lima/ipaffs/copied-from-guest/kubeconfig.yaml"
 
 # Deploy base services
+echo -e "${BLUE}:: Deploying base services to Kubernetes${NC}"
 kubectl apply -k "${REPO_DIR}/deploy"
+
+# Done \o/
+echo
+echo -e "${GREEN}:: IPAFFS development VM is provisioned and ready to use!${NC}"
+echo
+echo "Don't forget to configure kubectl and docker - consider adding these to your shell profile:"
+echo '  $ export KUBECONFIG="${HOME}/.lima/ipaffs/copied-from-guest/kubeconfig.yaml'
+echo '  $ export DEFRA_WORKSPACE="${HOME}/path/to/imports"'
+echo
 
 # vim: set ts=2 sts=2 sw=2 et:
--- scripts/setup-database.sh
+++ scripts/setup-database.sh
@@ -3,67 +3,99 @@
 REPO_DIR="$(cd "$(dirname $0)"/.. && pwd)"
 IMPORTS_DIR="${DEFRA_WORKSPACE}"
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 if ! [[ -d "${DEFRA_WORKSPACE}" ]]; then
-  echo "DEFRA_WORKSPACE environment variable not set." >&2
-  echo >&2
-  echo "Please set this to the directory where the \`imports\` repositories are checked out." >&2
+  echo -e "${RED}Error: DEFRA_WORKSPACE environment variable not set.${NC}" >&2
+  echo "Please specify the directory where the \`imports\` repositories are checked out, using the \`DEFRA_WORKSPACE\` environment variable." >&2
   echo "e.g. \`export DEFRA_WORKSPACE=/path/to/imports\`" >&2
+  echo >&2
   exit 1
 fi
 
 set -e
 
-override_db_port() {
-  export DATABASE_DB_HOST=127.0.0.1
-  export DATABASE_DB_PORT=31433
-  export DB_HOST=${DATABASE_DB_HOST}
-  export DB_PORT=${DATABASE_DB_PORT}
-  export DATABASE_DB_CONNECTION_STRING="jdbc:sqlserver://${DB_HOST}:${DB_PORT};database=${DB_NAME};encrypt=true;trustServerCertificate=${TRUST_SERVER_CERTIFICATE};hostNameInCertificate=*.database.windows.net;"
+export_db_conn() {
+  export _DB_USER="sa"
+  export _DB_PASS="dockerPassword1!"
+  export DB_HOST="127.0.0.1"
+  export DB_PORT=31433
+  export DATABASE_DB_HOST="${DB_HOST}"
+  export DATABASE_DB_PORT="${DB_PORT}"
+  export DATABASE_DB_CONNECTION_STRING="jdbc:sqlserver://${DB_HOST}:${DB_PORT};database=${DB_NAME};encrypt=true;trustServerCertificate=true;"
 }
 
 # Check prerequisites
 if ! command -v docker >/dev/null 2>&1; then
-  echo "\`docker\` not available in PATH. Please install Docker CLI" >&2
+  echo "${RED}Error: \`docker\` not available in PATH. Please install Docker CLI.${NC}" >&2
   exit 1
 fi
 if ! command -v bcp >/dev/null 2>&1; then
-  echo "\`bcp\` not available in PATH. Please install MSSQL Command Line Tools" >&2
+  echo "${RED}Error: \`bcp\` not available in PATH. Please install MSSQL Command Line Tools.${NC}" >&2
   exit 1
 fi
 if ! command -v sqlcmd >/dev/null 2>&1; then
-  echo "\`sqlcmd\` not available in PATH. Please install MSSQL Command Line Tools" >&2
+  echo "${RED}Error:\`sqlcmd\` not available in PATH. Please install MSSQL Command Line Tools.${NC}" >&2
   exit 1
 fi
 
 # Use correct Docker context
+echo -e "${BLUE}:: Switching Docker context to \`lima-ipaffs\`${NC}"
 docker context use lima-ipaffs
 
 # Build SQL Server container
-docker build --platform=linux/amd64 -t import-notification-database "${IMPORTS_DIR}/docker-local/database" 
+echo -e "${BLUE}:: Building database container image${NC}"
+docker build --platform=linux/amd64 -t import-notification-database "${IMPORTS_DIR}/docker-local/database"
 
 # Tag and push database container image
+echo -e "${BLUE}:: Pushing database container image to local registry${NC}"
 docker tag import-notification-database:latest host.docker.internal:30500/import-notification-database:latest
 docker push host.docker.internal:30500/import-notification-database:latest
 
 # Start the database container
+echo -e "${BLUE}:: Deploying the database service${NC}"
 kubectl apply -f "${REPO_DIR}/deploy/database/database.yaml"
 
 # Wait for MSSQL to start up
-echo "Waiting 2 minutes for database..."
-sleep 120
+echo -e "${BLUE}:: Waiting for database to become available${NC}"
+export_db_conn
+_starttime="$(date +%s)"
+while true; do
+  if sqlcmd -S "${DB_HOST},${DB_PORT}" -U "${_DB_USER}" -P "${_DB_PASS}" -Q "SELECT 1;" 2>/dev/null 1>/dev/null; then
+    echo -e "${GREEN}Database server is available${NC}"
+    break
+  fi
+  _curtime="$(date +%s)"
+  if [[ $((_curtime - _starttime)) -ge 300 ]]; then
+    echo -e "${RED}Error: Database service is not available.${NC}" >&2
+    exit 1
+  fi
+  sleep 5
+done
 
 # Source database go script and override database port number
 cd "${IMPORTS_DIR}/docker-local/database"
 source "${IMPORTS_DIR}/docker-local/database/go"
-override_db_port
+export_db_conn
 
 # Initialize the database
+echo -e "${BLUE}:: Initializing IPAFFS database${NC}"
 initialize_database
 
 # Source setup script and override database port number
 cd "${IMPORTS_DIR}/docker-local"
 source "${IMPORTS_DIR}/docker-local/docker_setup.sh"
-override_db_port
+export_db_conn
 
 # Load data
+echo -e "${BLUE}:: Loading fixture data${NC}"
 load_all_data
+
+# Done \o/
+echo -e "${GREEN}:: IPAFFS Database creation is complete!${NC}"
+
+# vim: set ts=2 sts=2 sw=2 et:
