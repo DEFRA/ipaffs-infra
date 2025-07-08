5c65a3ad0dcc4cca9f249e6ad25dcf81e811929a
richard.popple <2195350+richardpopple@users.noreply.github.com>
2025-07-08 13:06:14 +0100

    IMTA-20263 improving on script

--- migration-github/switch-dev-repos.sh
+++ migration-github/switch-dev-repos.sh
@@ -1,9 +1,16 @@
+NOT_TO_MIGRATE=(
+"git@giteux.azure.defra.cloud:imports/ansible.git",
+"git@giteux.azure.defra.cloud:imports/ansible-jenkins.git",
+"git@giteux.azure.defra.cloud:imports/terraform.git",
+"git@giteux.azure.defra.cloud:imports/ArchiveNotificationFunction.git"
+)
+
 echo This script will point existing local gitlab repos to the corresponding github repo.
 read -p "Do you wish to continue? <y/N> " PROMPT
-case $PROMPT in
-        [y]* ) break;;
-        * ) echo exiting; exit;;
-esac
+if [[ "${PROMPT}" -ne "y" ]]; then
+  echo exiting;
+  exit
+fi
 
 ## See if the DEFRA_WORKSPACE variable is set and that it is a valid path
 echo Checking DEFRA_WORKSPACE variable:
@@ -27,6 +34,7 @@ ssh -T git@github.com > /dev/null 2>&1
 RET=$?
 if [[ $RET == 1 ]]; then
   echo Authenticated - continuing
+  echo
 elif [[ $RET == 255 ]]; then
   echo User is not authenicated. 
   echo Please follow the instructions here to allow connecting via ssh 
@@ -40,31 +48,54 @@ else
   echo Unable to get a connection to github exiting
   exit
 fi
-## For each directory change the remote.
-cd $DEFRA_WORKSPACE
 
+## Make a backup.
+echo Making a backup.
+DEFRA_WORKSPACE_WITHOUT_TRALING_SLASH=$(echo ${DEFRA_WORKSPACE} | sed 's:/*$::')
+cp -R "${DEFRA_WORKSPACE_WITHOUT_TRALING_SLASH}" "${DEFRA_WORKSPACE_WITHOUT_TRALING_SLASH}"-BACKUP
+
+## TODO remove below
+cd /home/richard/Desktop/imports
+## TODO remove above
+
+## For each directory change the remote.
+cd "${DEFRA_WORKSPACE}"
 for DIRECTORY in */ ; do
     cd "${DIRECTORY}"
     echo Currently in directory: "${DIRECTORY}"
     CURRENT_REMOTE=$(git config --get remote.origin.url)
-    if [[ "${CURRENT_REMOTE}" == *"git@giteux.azure.defra.cloud:imports"* ]]; then
-      CURRENT_REPO_NAME=$(echo ${CURRENT_REMOTE} | sed 's:.*/::')
-      echo The current remote is: "${CURRENT_REMOTE}".
+    CURRENT_REPO_NAME=$(echo ${CURRENT_REMOTE} | sed 's:.*/::')
+    echo The current remote is: "${CURRENT_REMOTE}".
+    if [[ -z "${CURRENT_REMOTE}" ]]; then
+      ## Not a git repo
+      echo This is not a git repo. Skipping.
+    elif [[ ${NOT_TO_MIGRATE[@]} =~ "${CURRENT_REMOTE}" ]]; then
+      ## A git repo that we are not going to migrate.
+      echo Repo "${CURRENT_REPO_NAME}" is not to be migrated. Skipping.
+    elif [[ "${CURRENT_REMOTE}" == *"git@giteux.azure.defra.cloud:imports"* ]]; then
+      ## The default case.
       NEW_REMOTE="git@github.com:DEFRA/ipaffs-${CURRENT_REPO_NAME}"
-      read -p "The new remote will be: "${NEW_REMOTE}". Do you wish to continue? <y/N> " PROMPT
-      case $PROMPT in
-              [y]* )
-                git remote set-url origin ${NEW_REMOTE}
-                echo Switched remote to ${NEW_REMOTE} continuing;;
-              * )
-                echo Leaving remote as ${CURRENT_REMOTE}. Continuing;;
-      esac
+      ## We need to check for special case.
+      if [[ "${CURRENT_REMOTE}" == "git@giteux.azure.defra.cloud:imports/import-notification-schema.git" ]]; then
+        NEW_REMOTE="git@github.com:DEFRA/ipaffs-x-import-notification-schema"
+      fi
+      ## Set the new remote
+      git remote set-url origin ${NEW_REMOTE}
+      ## fetch from origin
+      git fetch > /dev/null 2>&1
+      RET=$?
+      if [[ "${RET}" -ne 0 ]]; then
+        echo There has been an error. The remote is set to ${NEW_REMOTE} but fetching failed.
+      else
+        ## Rename local master to main
+        git branch -m master main > /dev/null 2>&1
+        ## Set main to track main
+        git branch -u origin/main main > /dev/null 2>&1
+        echo The new remote is now "${NEW_REMOTE}", master has been renamed to main and track origin main.
+      fi
     else
-      echo The current remote is not pointing at git lab or this is not a git repo. Continuing.
+      echo This git repo is not set to track git lab. Skipping.
     fi
     echo
     cd ../
 done
-
-
-
