adef0fc95f72de1bee8c1b4bff2b565a8f20307f
Marcel <854060+marcelvdh@users.noreply.github.com>
2025-07-16 12:08:14 +0100

    Jenkins job migration script (#22)
    feat(migration): jenkins job migration script

---
+++ scripts/migration-github/jenkins-job-migration.sh
@@ -0,0 +1,201 @@
+#!/usr/bin/env bash
+set -euo pipefail
+#set -x
+
+# Constants
+readonly RED='\033[0;31m'
+readonly GREEN='\033[0;32m'
+readonly YELLOW='\033[1;33m'
+readonly BLUE='\033[0;34m'
+readonly NC='\033[0m'
+
+readonly PATH_EXCLUSION_MASK='*migrator*';
+
+readonly DOCUMENT1_XPATH1='/org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject/properties/org.jenkinsci.plugins.workflow.libs.FolderLibraries/libraries/org.jenkinsci.plugins.workflow.libs.LibraryConfiguration/retriever/scm/remote'
+readonly DOCUMENT1_XPATH2='/org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject/properties/org.jenkinsci.plugins.workflow.libs.FolderLibraries/libraries/org.jenkinsci.plugins.workflow.libs.LibraryConfiguration/retriever/scm/credentialsId'
+readonly DOCUMENT1_XPATH3='/org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject/sources/data/jenkins.branch.BranchSource/source/remote'
+readonly DOCUMENT1_XPATH4='/org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject/sources/data/jenkins.branch.BranchSource/source/credentialsId'
+readonly DOCUMENT1_XPATH5='/org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject/properties/org.jenkinsci.plugins.workflow.libs.FolderLibraries/libraries/org.jenkinsci.plugins.workflow.libs.LibraryConfiguration/defaultVersion'
+readonly DOCUMENT1_XPATH6='/org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject/sources/data/jenkins.branch.BranchSource/source/traits/jenkins.scm.impl.trait.RegexSCMHeadFilterTrait/regex'
+readonly DOCUMENT1_XPATH7='/flow-definition/definition/scm/userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/url'
+readonly DOCUMENT1_XPATH8='/flow-definition/definition/scm/userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/credentialsId'
+
+readonly DEFRA_GITHUB_PATH_PREFIX='https://github.com/DEFRA/ipaffs-'
+readonly CREDENTIALS_ID='github-migration-token'
+readonly DEFAULT_VERSION='main'
+
+readonly XPATHS=(
+  "$DOCUMENT1_XPATH1 $DOCUMENT1_XPATH2 $DOCUMENT1_XPATH3 $DOCUMENT1_XPATH4 $DOCUMENT1_XPATH5 $DOCUMENT1_XPATH6 $DOCUMENT1_XPATH7 $DOCUMENT1_XPATH8"
+)
+
+dry_run=0
+dirname=
+filename=
+
+# Script pre-requisites
+if [[ "${BASH_VERSINFO:-0}" -lt 4 ]]; then
+  echo -e "${RED}Error: this script requires Bash 4.0 or newer${NC}"
+fi
+if ! command -v xmlstarlet >/dev/null 2>&1; then
+  echo -e "${RED}Error: \`xmlstarlet\` not available in PATH. Please install XMLStarlet.${NC}" >&2
+  exit 1
+fi
+
+function usage() {
+  cat >&2 <<EOF
+Usage: $0 (-d /path/to/directory | -f /path/to/file) [-c /path/to/source/file] [-n]
+
+-d  Specify path to a directory in which to update job configurations
+-f  Specify single job configuration file to create/update
+-n  Dry run: show what would be done, but make no changes
+EOF
+}
+
+SED() {
+  if sed --version 2>/dev/null 1>/dev/null; then
+    # GNU
+    sed -E -i "${1}" "${2}"
+  else
+    # BSD
+    sed -E -i '' "${1}" "${2}"
+  fi
+}
+
+while getopts "d:f:n" opt; do
+  case $opt in
+    d)
+      dirname="${OPTARG}"
+      ;;
+    f)
+      filename="${OPTARG}"
+      ;;
+    n)
+      dry_run=1
+      ;;
+    \?)
+      usage
+      exit 0
+      ;;
+  esac
+done
+
+if [[ -z "${dirname}" && -z "${filename}" ]]; then
+  usage
+  exit 1
+fi
+if [[ -n "${dirname}" && -n "${filename}" ]]; then
+  echo -e "${RED}:: Can't specify both -d and -f${NC}" >&2;
+  exit 1
+fi
+if [[ -n "${dirname}" && ! -d "${dirname}" ]]; then
+  echo -e "${RED}:: \`${dirname}\` is not a directory${NC}" >&2;
+  exit 1
+fi
+if [[ -n "${filename}" && ! -f "${filename}" ]]; then
+  echo -e "${RED}:: \`${dirname}\` is not a filename${NC}" >&2;
+  exit 1
+fi
+
+function modify_version_start() {
+  SED 's/(<\?xml version=)(["'\''])1\.1/\1\21.0/' "${1}"
+}
+
+function modify_version_end() {
+  SED 's/(<\?xml version=)(["'\''])1\.0/\1\21.1/' "${1}"
+}
+
+function update_element() {
+  local docment_xpaths="${1:?"Value expected for argument: docment_xpaths"}"
+  local filename="${2?"Value expected for argument: filename"}"
+  local updatedElementValue=
+
+  trap 'modify_version_end "${filename}"' RETURN
+  modify_version_start "${filename}"
+
+  elementValue=$(xmlstarlet sel -t -v "$1" "$2")
+  if [[ -z "${elementValue:-}" ]]; then return 1; fi
+
+  case "${docment_xpaths##*/}" in
+    remote|url)
+      if ! [[ "${elementValue}" == "${DEFRA_GITHUB_PATH_PREFIX}"* ]]; then
+        updatedElementValue="${DEFRA_GITHUB_PATH_PREFIX}${elementValue##*/}"
+      else
+        updatedElementValue="${elementValue}"
+      fi
+      ;;
+    credentialsId)
+      updatedElementValue="${CREDENTIALS_ID}"
+      ;;
+    defaultVersion)
+      updatedElementValue="${DEFAULT_VERSION}"
+      ;;
+    regex)
+      updatedElementValue="${elementValue//master/main}"
+      ;;
+    *)
+      echo -e "${RED}:: Unmatched element ${elementValue}${NC}" >&2
+      exit 1
+      ;;
+  esac
+  if ! (( dry_run )); then
+    xmlstarlet ed -L -u "$1" -v "${updatedElementValue}" "${filename}"
+    echo "${updatedElementValue}"
+  else
+    echo "${updatedElementValue}"
+  fi
+
+  return 0
+}
+
+if [[ -n "${filename}" ]]; then
+  if ! ((dry_run)); then
+    random=$(( RANDOM % 100000 + 1 ))
+    cp "${filename}" "${filename}-$(date +%Y%m%d)-${random}"
+    echo -e ":: Created backup: ${filename}-$(date +%Y%m%d)-${random}"
+    echo -e ":: Processing file ${filename}"
+  fi
+
+  success=false
+  for docment_xpaths in "${XPATHS[@]}"; do
+    for xpath in ${docment_xpaths}; do
+       if result=$(update_element "${xpath}" "${filename}"); then
+        echo -e "${GREEN}:: Updated elementValue '${xpath##*/}' to value \`${result}\`${NC}"
+        success=true
+      fi
+    done
+  done
+  if [[ "${success}" == "false" ]]; then
+      echo -e "${BLUE}:: No elements to update have been found${NC}" >&2
+  fi
+  echo -e "Operation completed.\n"
+fi
+
+if [[ -n "${dirname}" ]]; then
+  if ! ((dry_run)); then
+    random=$(( RANDOM % 100000 + 1 ))
+    tarball="${dirname}-$(date +%Y%m%d)-${random}.tar.gz"
+    tar czf "${tarball}" "${dirname}"
+    echo -e ":: Created backup: ${tarball}"
+  fi
+
+  declare -i counter=0
+  declare -i skipped=0
+  while read -r file; do
+    echo -e ":: Processing file ${file}"
+      counter=$((counter + 1))
+      success=false
+      for docment_xpaths in "${XPATHS[@]}"; do
+        for xpath in ${docment_xpaths}; do
+           if result=$(update_element "${xpath}" "${file}"); then
+            echo -e "${GREEN}:: Updated element '${xpath##*/}' to value \`${result}\`${NC}"
+            success=true
+          fi
+        done
+      done
+      if [[ "${success}" == "false" ]]; then
+        echo -e "${BLUE}:: No elements to update have been found${NC}" >&2
+        skipped=$((skipped + 1))
+      fi
+  done < <(find "${dirname}" -type f -name "config.xml" ! -path "${PATH_EXCLUSION_MASK}") # don't subshell
+  echo -e "Operation completed.\n\nJobs Migrated : ${counter}\nJobs Skipped  : ${BLUE}${skipped}${NC}"
+fi
