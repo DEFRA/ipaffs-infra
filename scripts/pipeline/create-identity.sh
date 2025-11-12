b1cf7b5ad9091f42d93c8cda28086887b3d7a043
Tom Bamford <tom@bamford.io>
2025-11-12 13:16:21 +0000

    update webapp pipeline

--- scripts/pipeline/create-identity.sh
+++ scripts/pipeline/create-identity.sh
@@ -8,7 +8,7 @@ PRINCIPAL_ID="$(az identity show --name "${MANAGED_IDENTITY_NAME}" --resource-gr
 
 az identity federated-credential create --identity-name "${MANAGED_IDENTITY_NAME}" --resource-group "${RESOURCE_GROUP_NAME}" --name "${AKS_CREDENTIAL}" --issuer "${AKS_ISSUER}" --audiences "${AKS_AUDIENCES}" --subject "${AKS_SUBJECT}"
 
-echo "##vso[task.setvariable variable=clientId;isOutput=true]$CLIENT_ID"
-echo "##vso[task.setvariable variable=principalId]$PRINCIPAL_ID"
+echo "##vso[task.setvariable variable=clientId;isOutput=true]${CLIENT_ID}"
+echo "##vso[task.setvariable variable=principalId]${PRINCIPAL_ID}"
 
 # vim: set ts=2 sts=2 sw=2 et:
---
+++ scripts/pipeline/package-and-push-chart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux
+
+echo Logging in to ACR..
+az acr login --name "${REGISTRY_NAME}"
+
+echo Packaging Helm Chart...
+helm package "${CHART_PATH}" --version "${CHART_VERSION}"
+
+echo Pushing Helm chart to ACR...
+helm push "${CHART_NAME}-${CHART_VERSION}.tgz" "oci://${REGISTRY_HOST}/helm/v1/repo"
+
+# vim: set ts=2 sts=2 sw=2 et:
