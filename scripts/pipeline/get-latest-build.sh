b7feb0de407c08e6a09fe5234ad4ad9d7f25561b
Tom Bamford <tom@bamford.io>
2026-01-02 16:57:34 +0000

    error checking when getting latest build

--- scripts/pipeline/get-latest-build.sh
+++ scripts/pipeline/get-latest-build.sh
@@ -5,4 +5,9 @@ set -eux
 pipelineId="$(az pipelines show --org https://dev.azure.com/defragovuk/ --project DEFRA-EUTD-IPAFFS --folder-path "${FOLDER}" --name "${PIPELINE}" --query 'id')"
 latestBuildId="$(az pipelines runs list --org https://dev.azure.com/defragovuk/ --project DEFRA-EUTD-IPAFFS --query-order FinishTimeDesc --result succeeded --status completed --top 1 --pipeline-ids "${pipelineId}" --branch "${BRANCH}" --query '[0].id')"
 
+if [[ "${latestBuildId}" == "" ]]; then
+  echo "No build was found" >&2
+  exit 1
+fi
+
 echo "##vso[task.setvariable variable=version;isOutput=true]${latestBuildId}"
