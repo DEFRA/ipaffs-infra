ccff5c1d498aa5d9bd44319d14c1167c731ccdcb
Tom Bamford <tom@bamford.io>
2026-01-02 12:27:41 +0000

    query for first build ID instead of barfing entire object array

--- scripts/pipeline/get-latest-build.sh
+++ scripts/pipeline/get-latest-build.sh
@@ -3,6 +3,6 @@
 set -eux
 
 pipelineId="$(az pipelines show --org https://dev.azure.com/defragovuk/ --project DEFRA-EUTD-IPAFFS --folder-path "${FOLDER}" --name "${PIPELINE}" --query 'id')"
-latestBuildId="$(az pipelines runs list --org https://dev.azure.com/defragovuk/ --project DEFRA-EUTD-IPAFFS --query-order FinishTimeDesc --result succeeded --status completed --top 1 --pipeline-ids "${pipelineId}" --branch "${BRANCH}")"
+latestBuildId="$(az pipelines runs list --org https://dev.azure.com/defragovuk/ --project DEFRA-EUTD-IPAFFS --query-order FinishTimeDesc --result succeeded --status completed --top 1 --pipeline-ids "${pipelineId}" --branch "${BRANCH}" --query '[0].id')"
 
 echo "##vso[task.setvariable variable=version;isOutput=true]${latestBuildId}"
