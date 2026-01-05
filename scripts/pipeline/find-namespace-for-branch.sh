075b41914bc467dd41325fd9f7028c54efbccfa9
Tom Bamford <tom@bamford.io>
2026-01-05 11:17:26 +0000

    bashism

--- scripts/pipeline/find-namespace-for-branch.sh
+++ scripts/pipeline/find-namespace-for-branch.sh
@@ -2,14 +2,14 @@
 
 set -eux
 
-if [[ "${BRANCH}" -eq "${TRUNK}" ]]; then
+if [[ "${BRANCH}" == "${TRUNK}" ]]; then
   namespace="$(echo "${ENVIRONMENT}" | tr '[:upper:]' '[:lower:]')"
 else
   encodedBranch="$(echo -n "${BRANCH}" | base64 | tr -d '=')"
   namespace="$(kubectl get namespace -l "defra.gov.uk/branch=${encodedBranch}" -o json | jq -r '.items[0].metadata.name')"
 fi
 
-if [[ -z "${namespace}" ]] || [[ "${namespace}" -eq "null" ]]; then
+if [[ "${namespace}" == "" ]] || [[ "${namespace}" == "null" ]]; then
   echo "No namespace found for branch \`${BRANCH}\`" >&2
   exit 1
 fi
