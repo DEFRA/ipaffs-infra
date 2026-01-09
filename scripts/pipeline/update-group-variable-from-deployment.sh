f751b95d47bc58cec8d4188a9a184d0bcdf54b04
Tom Bamford <tom@bamford.io>
2026-01-09 01:10:00 +0000

    Add Entra groups, runtime bicep params, some refactoring
    - Add Entra groups for Azure Search
    - Add service identities to SearchContributors group
    - Save group details to ADO library group
    - Some overdue refactoring of pipeline steps into stages

---
+++ scripts/pipeline/update-group-variable-from-deployment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eux
+
+DIR="$(cd "$(dirname $0)"/.. && pwd)"
+
+deploymentOutput() {
+  az deployment group show \
+    --name "${DEPLOYMENT_NAME}" \
+    --resource-group "${RESOURCE_GROUP}" \
+    --query properties.outputs.${1}.value \
+    -o tsv
+}
+
+valueToSet="$(deploymentOutput "${2}")"
+"${DIR}"/update-group-variable.sh "${1}" "${valueToSet}"
+
+# vim: set ts=2 sts=2 sw=2 et:
