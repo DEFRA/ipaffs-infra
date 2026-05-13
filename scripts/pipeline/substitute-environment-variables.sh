133e44ee9cf537f629f66ca072ce5b20a047b2f3
Marcel van den Hof <854060+marcelvdh@users.noreply.github.com>
2026-05-13 17:25:42 +0100

    Cleanup script

--- scripts/pipeline/substitute-environment-variables.sh
+++ scripts/pipeline/substitute-environment-variables.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# usage $0 [.env file] [KV] [output .env file]
+## substitute-environment-variables.sh
+##
+## Substitutes environment variable tokens with values stored in Azure Key Vault
+##
+## usage $0 [env file to parse] [Azure Key Vault] [name of env file]
+## Required arguments
+## [$1] - The .env file to process
+## [$2] - The name of the Azure Key Vault to retrieve tokens from
+## [$3] - The name of .env file to output the substituted results to
 
 SCRIPTS_DIR="$(cd "$(dirname $0)"/.. && pwd)"
 AKV_KEYS=$(sed -n 's/.*${\([^}]*\)}.*/\1/p' "${1}" | sort -u)
@@ -19,4 +27,4 @@ done
 
 SHELL_FORMAT=$(printf '${%s} ' ${AKV_KEYS})
 envsubst "${SHELL_FORMAT}" < "${1}" > "${3}"
-echo -e "$:: Writing updated environment file ${3}"
+echo -e ":: Writing updated environment file ${3}"
